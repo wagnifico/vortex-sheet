77f7f0cff1746929d679f246fc36f2b2c9b32deb
Wagner Gonçalves Pinto <73096490+wagnifico@users.noreply.github.com>
2024-03-03 00:45:05 +0100

    improve plot

--- vortex.py
+++ vortex.py
@@ -14,7 +14,7 @@ halfN = 1.0/(2.0*N)
 eps2 = epsilon**2
 twopi = 2*np.pi
 
-domain_size = 1.0
+domain_size = 2.0
 
 intial_amplitude = 0.01
 initial_wave = intial_amplitude*np.sin(twopi*np.linspace(0,domain_size,N))
@@ -24,11 +24,12 @@ y_0 = -initial_wave
 
 s_0 = np.concatenate((x_0,y_0))
 
+
 def rhs(t,s,):
     x, y = s[:N], s[N:]
     dsdt = np.zeros(2*N)
 
-    for i in range(N):        
+    for i in range(N):
         denominator = 1.0/ \
             (np.cosh(twopi*(y[i] - y)) - np.cos(twopi*(x[i] - x)) + eps2)
         # no need to remove self-induction singularity if eps > 0
@@ -48,17 +49,24 @@ sol = RK45(
     vectorized=True
     )
 
+fig, ax = plt.subplots(
+    1,1,layout='constrained',figsize=(16/2.54,6/2.54))
 
-fig, ax = plt.subplots(1,1,layout='constrained')
-
+ax.axis('equal')
 ax.set_ylim([-0.3,0.3])
+ax.set_xlim([0,domain_size])
 
 i = 0
 t = t_start
 
 files = []
 while sol.t <= t_end - dt:
-    ax.plot(sol.y[:N],sol.y[N:],'-ok')
+    ax.plot(
+        sol.y[:N],sol.y[N:],
+        '-ok',
+        linewidth=0.75,
+        markersize=2,
+        )
     ax.set_title(f't = {sol.t:0.3f}')
 
     filename = f'output/solution_{i:04d}.png'
@@ -66,9 +74,9 @@ while sol.t <= t_end - dt:
     files.append(filename)
 
     i += 1
-    print(f'{i:5>d} : {sol.t:.5e}')
+    print(f'{i:>5d} : {sol.t:.5e}')
     sol.step()
-    
+
     ax.lines[0].remove()
 
 
